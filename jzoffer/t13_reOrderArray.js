4cd030eca24d7e961096ca80a4f97c2a6c6d219e
Si3ver <zhou_weilin@outlook.com>
2018-09-29 21:04:08 +0800

    add comparePKP.js

--- jzoffer/t13_reOrderArray.js
+++ jzoffer/t13_reOrderArray.js
@@ -5,6 +5,11 @@ function reOrderArray(arr)
     if(!(arr instanceof Array)){
         return false;
     }
-    // 待补充
-
+    var lIdx = 0, rIdx = arr.length-1;
+    while(lIdx < rIdx){
+        // if 
+    }
+    
 }
+
+console.log(reOrderArray([213,324,43,574,865,345,234,124,7245,845,4578,325]));
